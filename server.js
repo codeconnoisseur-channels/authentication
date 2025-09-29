e4024c1a03f137f55ceaee25c741d27cfecf4f3c
codeconnoisseur-channels <channelsokunade@gmail.com>
2025-09-29 16:48:07 +0100

    Allowed cors origin

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("./config/database");
 const app = express();
 const userRouter = require("./routes/userRouter");
 app.use(express.json());
+app.use(cors("*"));
 app.use(userRouter);
 const port = process.env.PORT;
 app.listen(port, () => {
