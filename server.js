34a4dfeedec4aa3171773378f1503b2af0570810
codeconnoisseur-channels <channelsokunade@gmail.com>
2025-09-29 17:12:23 +0100

    cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./config/database");
 const app = express();
+const cors = require("cors");
 const userRouter = require("./routes/userRouter");
 app.use(express.json());
 app.use(cors("*"));
